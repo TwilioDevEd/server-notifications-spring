cf03fa030e44d351cccd6007bcc56674a9829528
Agustin Camino <agustin.camino@gmail.com>
2015-09-25 15:13:32 -0500

    Add administrator model

---
+++ src/main/java/com/twilio/notifications/domain/Administrator.java
@@ -0,0 +1,23 @@
+package com.twilio.notifications.domain;
+
+public class Administrator {
+    private String name;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
